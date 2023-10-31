04874771c494fc390ad0271407cdf3519769ac50
Jacob Vosmaer <contact@jacobvosmaer.nl>
2023-10-31 14:11:40 +0100

    Count frames in remaining variable

--- Mangler.cpp
+++ Mangler.cpp
@@ -6,7 +6,7 @@ using namespace daisy;
 DaisyPod hw;
 Parameter p_knob1, p_knob2;
 
-#define nelem(x)(sizeof(x)/sizeof(*x))
+#define nelem(x) (sizeof(x) / sizeof(*x))
 
 float DSY_SDRAM_BSS buffer[30 * 48000 * 2]; /* 30s at 48kHz */
 
@@ -23,19 +23,19 @@ static void AudioCallback(AudioHandle::InterleavingInputBuffer in,
   size_t to_buffer = size, from_buffer = size;
   size_t remaining;
 
-  if (remaining = buf.end - buf.head, to_buffer > remaining / 2) {
-    memcpy(buf.head, in, remaining * sizeof(*in));
-    to_buffer -= remaining / 2;
-    in += remaining;
+  if (remaining = (buf.end - buf.head) / 2, to_buffer > remaining) {
+    memcpy(buf.head, in, 2 * remaining * sizeof(*in));
+    to_buffer -= remaining;
+    in += 2 * remaining;
     buf.head = buf.buffer;
   }
   memcpy(buf.head, in, 2 * to_buffer * sizeof(*in));
   buf.head += 2 * to_buffer;
 
-  if (remaining = buf.end - buf.tail, from_buffer > remaining / 2) {
-    memcpy(out, buf.tail, remaining * sizeof(*out));
-    from_buffer -= remaining / 2;
-    out += remaining;
+  if (remaining = (buf.end - buf.tail) / 2, from_buffer > remaining) {
+    memcpy(out, buf.tail, 2 * remaining * sizeof(*out));
+    from_buffer -= remaining;
+    out += 2 * remaining;
     buf.tail = buf.buffer;
   }
   memcpy(out, buf.tail, 2 * from_buffer * sizeof(*out));
