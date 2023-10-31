5a3d0a19d2a7c2661919183df33d2812082840d1
Jacob Vosmaer <contact@jacobvosmaer.nl>
2023-10-31 15:27:35 +0100

    Remove LED code

--- Mangler.cpp
+++ Mangler.cpp
@@ -4,7 +4,6 @@
 using namespace daisy;
 
 DaisyPod hw;
-Parameter p_knob1, p_knob2;
 
 #define nelem(x) (sizeof(x) / sizeof(*x))
 
@@ -44,20 +43,9 @@ static void AudioCallback(AudioHandle::InterleavingInputBuffer in,
 
 int main(void) {
   hw.Init();
-  float r = 0, g = 0, b = 0;
-  p_knob1.Init(hw.knob1, 0, 1, Parameter::LINEAR);
-  p_knob2.Init(hw.knob2, 0, 1, Parameter::LINEAR);
   hw.SetAudioBlockSize(4);
-
-  hw.StartAdc();
   hw.StartAudio(AudioCallback);
 
-  while (1) {
-    r = p_knob1.Process();
-    g = p_knob2.Process();
-
-    hw.led1.Set(r, g, b);
-
-    hw.UpdateLeds();
-  }
+  while (1)
+    ;
 }
