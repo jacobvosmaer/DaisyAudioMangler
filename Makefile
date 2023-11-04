04483960ae4b946df8dec2c3f62e24cfa1035511
Jacob Vosmaer <contact@jacobvosmaer.nl>
2023-11-04 19:32:40 +0100

    It scrubs

--- Makefile
+++ Makefile
@@ -7,8 +7,6 @@ CPP_SOURCES = Mangler.cpp
 # Library Locations
 LIBDAISY_DIR = ./libDaisy
 
-CFLAGS = -Werror -pedantic
-
 # Core location, and generic Makefile.
 SYSTEM_FILES_DIR = $(LIBDAISY_DIR)/core
 include $(SYSTEM_FILES_DIR)/Makefile
--- Mangler.cpp
+++ Mangler.cpp
@@ -12,7 +12,7 @@ float DSY_SDRAM_BSS
     buffer[(1 << 26) / sizeof(float)]; /* Use all 64MB of sample RAM */
 
 struct {
-  float *start, *end, *write, *read, read_frac;
+  float *start, *end, *write, *read, read_frac, scrub, scrub_origin;
   int dir;
 } buf;
 
@@ -27,7 +27,7 @@ void buf_init(void) {
   memset(buffer, 0, sizeof(buffer));
 }
 
-float *bufWrap(float *p) {
+float *buf_wrap(float *p) {
   ptrdiff_t buf_size = buf.end - buf.start;
   if (p < buf.start)
     p += buf_size;
@@ -35,6 +35,9 @@ float *bufWrap(float *p) {
     p -= buf_size;
   return p;
 }
+
+float combine(float f, float x, float y) { return f * x + (1.0 - f) * y; }
+int fsign(float f) { return f > 0 ? 1 : -1; }
 
 static void AudioCallback(AudioHandle::InterleavingInputBuffer in,
                           AudioHandle::InterleavingOutputBuffer out,
@@ -45,33 +48,58 @@ static void AudioCallback(AudioHandle::InterleavingInputBuffer in,
   if (new_dir)
     buf.dir = new_dir;
 
-  if (hw.button1.FallingEdge()) {
-    /* button1 was pressed so buf.read is possibly desynchronized from
-     * buf.write. Reset so there is no audio lag during normal playback. */
+  if (hw.button1.FallingEdge() || hw.button2.FallingEdge()) {
+    /* We are returning to normal playback but buf.read is possibly
+     * desynchronized from buf.write. */
     buf.read = buf.write;
     buf.read_frac = 0;
+  }
+
+  if (hw.button2.RisingEdge()) { /* start scrubbing mode */
+    buf.scrub = 0;
+    buf.scrub_origin = hw.knob2.Process();
   }
 
   for (int i = 0; i < (int)size; i += 2) {
     for (int j = 0; j < 2; j++)
       buf.write[j] = in[i + j];
-    buf.write = bufWrap(buf.write + 2);
+    buf.write = buf_wrap(buf.write + 2);
   }
 
   if (hw.button1.Pressed()) {
     float speed = hw.knob1.Process();
     for (int i = 0; i < (int)size; i += 2) {
       for (buf.read_frac += speed; buf.read_frac > 1.0; buf.read_frac -= 1.0)
-        buf.read = bufWrap(buf.read + buf.dir * 2);
+        buf.read = buf_wrap(buf.read + buf.dir * 2);
       for (int j = 0; j < 2; j++)
-        out[i + j] = buf.read_frac * buf.read[j] +
-                     (1.0 - buf.read_frac) * bufWrap(buf.read - buf.dir * 2)[j];
+        out[i + j] =
+            buf.read_frac * buf.read[j] +
+            (1.0 - buf.read_frac) * buf_wrap(buf.read - buf.dir * 2)[j];
     }
+  } else if (hw.button2.Pressed()) {
+    float new_scrub = hw.knob2.Process() - buf.scrub_origin;
+    float scrub_range_seconds = 1.0;
+    float sample_rate = hw.AudioSampleRate();
+    float old_sample = buf.scrub * scrub_range_seconds * sample_rate;
+    float new_sample = new_scrub * scrub_range_seconds * sample_rate;
+
+    float step = (new_sample - old_sample) / (float)(size / 2);
+    float sample = old_sample;
+    for (int i = 0; i < (int)size; i += 2, sample += step) {
+      float *read =
+          buf_wrap(buf.read + 2 * fsign(sample) * (int)floorf(sample));
+      float read_frac = sample - floorf(sample);
+      for (int j = 0; j < 2; j++)
+        out[i + j] =
+            combine(read_frac, *buf_wrap(read + j), *buf_wrap(read + 2 + j));
+    }
+
+    buf.scrub = new_scrub;
   } else {
     for (int i = 0; i < (int)size; i += 2) {
       for (int j = 0; j < 2; j++)
         out[i + j] = buf.read[j];
-      buf.read = bufWrap(buf.read + 2);
+      buf.read = buf_wrap(buf.read + 2);
     }
   }
 }
@@ -80,7 +108,6 @@ int main(void) {
   hw.Init();
   buf_init();
   hw.StartAdc();
-  hw.SetAudioBlockSize(4);
   hw.StartAudio(AudioCallback);
 
   while (1)
