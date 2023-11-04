2095130564d220238b0f61a3913860aa89c43bd2
Jacob Vosmaer <contact@jacobvosmaer.nl>
2023-11-04 20:09:17 +0100

    Add buf_add

--- Mangler.cpp
+++ Mangler.cpp
@@ -37,6 +37,12 @@ float *buf_wrap(float *p) {
   return p;
 }
 
+float *buf_add(float **p, ptrdiff_t n) {
+  float *ret = *p;
+  *p = buf_wrap(*p + n);
+  return ret;
+}
+
 float combine(float f, float x, float y) { return f * x + (1.0 - f) * y; }
 int fsign(float f) { return f > 0 ? 1 : -1; }
 
@@ -62,18 +68,13 @@ static void AudioCallback(AudioHandle::InterleavingInputBuffer in,
     buf.scrub = 0;
   }
 
-  for (int i = 0; i < (int)size; i += nchan) {
-    for (int j = 0; j < nchan; j++)
-      buf.write[j] = in[i + j];
-    buf.write = buf_wrap(buf.write + nchan);
-  }
+  for (int i = 0; i < (int)size; i++)
+    *buf_add(&buf.write, 1) = in[i];
 
   if (hw.button1.Pressed()) { /* variable speed playback */
     float speed = hw.knob1.Process();
-    for (int i = 0; i < (int)size; i += nchan) {
-      buf.read_frac += speed;
-      buf.read =
-          buf_wrap(buf.read + nchan * buf.dir * (int)floorf(buf.read_frac));
+    for (int i = 0; i < (int)size; i += nchan, buf.read_frac += speed) {
+      buf_add(&buf.read, nchan * buf.dir * (int)floorf(buf.read_frac));
       buf.read_frac -= floorf(buf.read_frac);
       for (int j = 0; j < nchan; j++)
         out[i + j] = combine(buf.read_frac, buf.read[j],
@@ -93,15 +94,11 @@ static void AudioCallback(AudioHandle::InterleavingInputBuffer in,
           buf_wrap(buf.read + nchan * fsign(sample) * (int)floorf(sample));
       float read_frac = sample - floorf(sample);
       for (int j = 0; j < nchan; j++)
-        out[i + j] =
-            combine(read_frac, buf_wrap(read)[j], buf_wrap(read + nchan)[j]);
+        out[i + j] = combine(read_frac, read[j], buf_wrap(read + nchan)[j]);
     }
   } else { /* normal playback */
-    for (int i = 0; i < (int)size; i += nchan) {
-      for (int j = 0; j < nchan; j++)
-        out[i + j] = buf.read[j];
-      buf.read = buf_wrap(buf.read + nchan);
-    }
+    for (int i = 0; i < (int)size; i++)
+      out[i] = *buf_add(&buf.read, 1);
   }
 }
 
